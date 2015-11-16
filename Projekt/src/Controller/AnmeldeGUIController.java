851e74919f4abfbe8bc915a721743733b5b1b26b
corrothezorro <corrothezorro@googlemail.com>
2015-11-16 15:20:31 +0100

    Update AnmeldeGUIController.java
    connection_update

--- Projekt/src/Controller/AnmeldeGUIController.java
+++ Projekt/src/Controller/AnmeldeGUIController.java
@@ -21,6 +21,42 @@ public class AnmeldeGUIController {
 	}
 	
 	public Connection getConnection(){
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your Oracle JDBC Driver?");
+			e.printStackTrace();
+			
+
+		}
+
+		System.out.println("Oracle JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:oracle:thin:@aix1.fh-bielefeld.de:1521:d2", "dvi992",
+					"fh2274");
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	
 		return null;
 		
 	}
