d8a37290bbf51e1b62be962403654c62d883c81c
User <timo.nguyen@fh-bielefeld.de>
2015-11-16 18:20:27 +0100

    ein zweiter Test

--- Projekt/src/Controller/UserDetController.java
+++ Projekt/src/Controller/UserDetController.java
@@ -3,5 +3,6 @@ package Controller;
 public class UserDetController {
 	public static void main(String[] args) {
 		System.out.println("Das ist ein Test.");
+		System.out.println("Test Test Test");
 	}
 }
